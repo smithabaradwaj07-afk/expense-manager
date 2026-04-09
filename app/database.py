8c56857ec6e8cd579cc97c11e332a9059bff232e
smitha1605 <smithabharadwaj10@gmail.com>
2026-04-09 16:10:15 +0530

    updated db connection to supabase

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:pass123@localhost/expense_db"
+DATABASE_URL = "postgresql://postgres:Samhitha%40123@db.gfvudjxvynezkxdfsjgu.supabase.co:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 
